5e12b4f582c10b3dbca62b9fd1f4f529410fd97b
Artur <sarkazmus1177@gmail.com>
2022-11-03 14:22:01 +0300

    dz

---
+++ loader/view.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, render_template, request
+from utils import *
+import logging
+
+loader_blueprint = Blueprint('loader_blueprint', __name__, template_folder='templates', url_prefix='/')
+logging.basicConfig(filename='basic.log', level=logging.INFO)
+errors_blueprint = Blueprint('errors_blueprint', __name__, template_folder='templates')
+logging.basicConfig(filename='basic.log', level=logging.ERROR)
+
+@loader_blueprint.route('/post')
+def loader_page():
+    return render_template('post_form.html')
+
+@loader_blueprint.route('/post', methods=['POST'])
+def upload_post():
+    picture = request.files.get('picture')
+    content = request.form.get('content')
+
+    picture_url = save_picture(picture)
+
+    if not picture_url:
+        logging.info(f'{picture_url}: Не изображение!')
+        return 'Не изображение!'
+
+    add_post({'pic': picture_url, 'content': content})
+
+    return render_template('post_uploaded.html', picture_url=picture_url, content=content)
+
+@errors_blueprint.errorhandler(404)
+def not_found(e):
+    return "Ошибка при загрузке!"
---
+++ main/view.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template, request
+from utils import *
+
+main_blueprint = Blueprint('main_blueprint', __name__, template_folder='templates', url_prefix='/')
+
+@main_blueprint.route('/')
+def main_page():
+    return render_template('index.html')
+
+@main_blueprint.route('/search')
+def search_page():
+    substr = request.args.get('s')
+    posts = search_post(substr)
+    return render_template('post_list.html', posts=posts, substr=substr)
